964777570b8bdd4e741984c15a841966ab5b6ddd
nayeon0731 <toothless1596@gmail.com>
2022-06-15 17:06:17 +0900

    todoapp

--- 05/quiz3.js
+++ 05/quiz3.js
@@ -1,24 +1,86 @@
-console.log('quiz3');
+var $btnAddRed = document.querySelector(".add-red");
+var $btnAdd = document.querySelector(".add-btn");
+var $btnDelete = document.querySelector(".delete-btn");
+var $btnText = document.querySelector(".text-btn");
+var $btnReset = document.querySelector(".reset-btn");
+var $btnToggle = document.querySelector(".toggle-btn");
+var $btnImg = document.querySelector(".image-btn");
+var $textbox = document.querySelector(".txt");
 
-var addRedBtn = document.querySelector('.add-red');
-var addBtn = document.querySelector('.add-btn');
-var delBtn = document.querySelector('.delete-btn');
-var box = document.querySelector('.box');
+function addRed() {
+    var $boxes = document.querySelectorAll('.box');
+    $boxes.forEach(function ($box) {
+        $box.classList.add('red');
+    });
+}
 
 
-addRedBtn.addEventListener('click', function () {
-    box.style.backgroundColor = 'red';
-});
+function addBox() {
+    var $currentBox = document.querySelector('.box');
+    var $newBox;
+    if ($currentBox) {
+        $newBox = $currentBox.cloneNode(true);
+        // console.log($newBox);
+        document.body.appendChild($newBox);
+    }
+    else {
+        $newBox = createBox();
+    }
+}
 
-addBtn.addEventListener('click', function() {
-    const addBox = document.createElement('div');
-    addBox.className = 'box';
-    document.body.appendChild(addBox);
-    console.log('div');
-});
+function delBox() {
+    var $box = document.querySelector('.box');
+    if ($box) $box.remove();
+}
 
-delBtn.addEventListener('click', function() {
-    //삭제할 box들이 있을 때만 삭제되도록. 
-    addBox.removeChild(addBox.children[0]);
+function addText() {
+    var text = $textbox.value;
+    var $boxes = document.querySelectorAll('.box');
+    $boxes.forEach(function ($box) {
+        $box.innerHTML = text;
+    });
+}
 
-});
+function resetBox() {
+    var $boxes = document.querySelectorAll('.box');
+    $boxes.forEach(function ($box) {
+        $box.remove();
+    });
+}
+
+function toggleBox() {
+    var $boxes = document.querySelectorAll('.box');
+    $boxes.forEach(function ($box) {
+        $box.hidden = !$box.hidden;
+    });
+}
+
+function addImg() {
+    var $boxes = document.querySelectorAll('.box');
+    var img = "<img src='https://i.imgur.com/69NjYBH.png', alt='star_image'></img>";
+    var $newBox;
+    if ($boxes) {
+        $boxes.forEach(function ($box) {
+            $box.innerHTML = img;
+        });
+    }
+    else {
+        $newBox = createBox();
+        $newBox.innerHTML = img;
+    }
+}
+
+function createBox() {
+    $newBox = document.createElement("div");
+    $newBox.classList.add('box');
+    document.body.appendChild($newBox);
+    return $newBox;
+}
+
+$btnAddRed.addEventListener('click',addRed);
+$btnAdd.addEventListener('click',addBox);
+$btnDelete.addEventListener('click',delBox);
+$btnText.addEventListener('click',addText);
+$btnReset.addEventListener('click',resetBox);
+$btnToggle.addEventListener('click',toggleBox);
+$btnImg.addEventListener('click',addImg);
---
+++ 13/todoapp/src/index.js
@@ -0,0 +1,38 @@
+import { log } from './utils';
+import { printTodos } from './print-todos';
+import { init as initForm } from './form';
+import './todos.css';
+import { get as getStorage } from './storage';
+
+const todos = getStorage() || [];
+
+const deleteTodo = (index) => {
+  console.log('delete', index);
+  // todos에서 index번째 todo 삭제
+  todos.splice(index, 1);
+  print();
+};
+
+const print = () => {
+  printTodos(todos);
+};
+const toggleTodo = (index) => {
+  console.log('toggle');
+  // index에 맞는 todo.isDone 반전
+  todos[index].isDone = !todos[index].isDone;
+  print();
+};
+
+// 삭제,isDone
+document.body.addEventListener('click', (event) => {
+  const index = parseInt(event.target.parentNode.dataset.index, 10);
+  if (event.target.className === 'delete') {
+    deleteTodo(index);
+  } else if (event.target.className === 'toggle-checked') {
+    toggleTodo(index);
+  }
+});
+
+initForm(todos);
+// todos 출력
+print();
